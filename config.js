8d626ff9d293759351a68fbe569ac79902311ed9
first29 <90001623+first29@users.noreply.github.com>
2024-05-01 01:17:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ export const config = {
   host: process.env.DB_HOST||"127.0.0.1",
   port: process.env.DB_PORT||"3306",
   user: process.env.DB_USER||"R",
-  password: process.env.DB_PASSWORD||"5612633",
+  password: process.env.DB_PASSWORD||"***",
   database: process.env.DB_NAME||"r",
 };
